6d5d5e038cabf4f280faab508bcac525e8971d19
Rodrigo <www.rodrigogarcia10@hotmail.com>
2025-11-18 23:24:07 -0300

    feito

--- src/main/java/com/example/MirageServer/dto/response/UserResponseMeDTO.java
+++ src/main/java/com/example/MirageServer/dto/response/UserResponseMeDTO.java
@@ -1,6 +1,5 @@
 package com.example.MirageServer.dto.response;
 
-import java.time.Instant;
 import java.util.Date;
 
 public record UserResponseMeDTO (Long id, String name, String email, Date Created_at) {
